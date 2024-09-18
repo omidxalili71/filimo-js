0af686c71456a8875d6e5a03f4416083b9321962
Omid Khalili <o.khalili@PSP.local>
2024-09-18 10:18:49 +0330

    generate-api

--- public/js/Menu/Menu.js
+++ public/js/Menu/Menu.js
@@ -2,7 +2,7 @@ async function Menu() {
   let menu = "";
   let data = await fetch("http://localhost:3000/menu");
   let res = await data.json();
-  
+
   menu = res.map((item) => {
     return `<div class="menu__item">
               <a href="#" id="menu__link">
@@ -10,8 +10,7 @@ async function Menu() {
               ${item.name}</a>
             </div>`;
   });
-  
-  
+
   document
     .querySelector(".menu__nav")
     .insertAdjacentHTML("beforeend", menu.join(""));
