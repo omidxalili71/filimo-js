ac99c70b16398589fcb1ae1f267d51038d86a027
omidxalili71 <omxalili@gmail.com>
2024-09-18 11:44:17 +0330

    Update Menu.js

--- public/js/Menu/Menu.js
+++ public/js/Menu/Menu.js
@@ -1,6 +1,6 @@
 async function Menu() {
   let menu = "";
-  let data = await fetch("https://omidxalili71.github.io/api/menu.json");
+  let data = await fetch("../../../menu.json");
   let res = await data.json();
 
   menu = res.map((item) => {
